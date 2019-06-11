39d7af015c2ac477f8c2158ae9c315e387892f33
Cam McLeod <cammcleod3@gmail.com>
2019-06-10 19:56:17 -0700

    images added to view and can scroll between them, locking on images

--- ScrollView-Image-Galleries/ViewController.m
+++ ScrollView-Image-Galleries/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () < UIScrollViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
@@ -18,40 +18,82 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+//    // Do any additional setup after loading the view.
     _scrollView = self.scrollView;
-    
+    self.scrollView.pagingEnabled = YES;
+    self.scrollView.showsHorizontalScrollIndicator = NO;
+
     self.scrollView.delegate = self;
-    
+    [self.view addSubview:self.scrollView];
     self.scrollView.maximumZoomScale = 1.0;
     self.scrollView.minimumZoomScale = 0.5;
     
     UIImageView *firstImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed: @"Lighthouse-in-Field"]];
-    
+
     UIImageView *secondImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed: @"Lighthouse-night"]];
-    
+
     UIImageView *thirdImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed: @"Lighthouse-zoomed"]];
     
-    [self.scrollView addSubview:firstImageView];
-    [self.scrollView addSubview:secondImageView];
-    [self.scrollView addSubview:thirdImageView];
+        [self.scrollView addSubview:firstImageView];
+        [self.scrollView addSubview:secondImageView];
+        [self.scrollView addSubview:thirdImageView];
     
-    secondImageView.frame = CGRectOffset(secondImageView.frame, firstImageView.frame.size.width, 0);
-    thirdImageView.frame = CGRectOffset(thirdImageView.frame, secondImageView.frame.size.width, 0);
-    NSLog(@"%@", NSStringFromCGSize( self.scrollView.contentSize));
+    firstImageView.contentMode = UIViewContentModeScaleAspectFit;
+    secondImageView.contentMode = UIViewContentModeScaleAspectFit;
+    thirdImageView.contentMode = UIViewContentModeScaleAspectFit;
+
+    CGFloat startingPointOfX = 0;
+    
+    firstImageView.frame = CGRectOffset(firstImageView.frame,startingPointOfX, 0);
+    startingPointOfX += firstImageView.frame.size.width;
+    secondImageView.frame = CGRectOffset(secondImageView.frame,startingPointOfX, 0);
+    startingPointOfX += secondImageView.frame.size.width;
+    thirdImageView.frame = CGRectOffset(thirdImageView.frame,startingPointOfX, 0);
+    startingPointOfX += thirdImageView.frame.size.width;
+    
+    self.scrollView.contentSize = CGSizeMake(startingPointOfX, CGRectGetHeight(self.scrollView.frame));
+    
+    
+    NSLayoutConstraint *firstImageViewCenterYConstraint = [NSLayoutConstraint constraintWithItem: firstImageView
+                                                                                       attribute:NSLayoutAttributeCenterY
+                                                                                       relatedBy:NSLayoutRelationEqual
+                                                                                          toItem:self.scrollView
+                                                                                       attribute:NSLayoutAttributeCenterY
+                                                                                      multiplier:1.0
+                                                                                        constant:0.0];
+    firstImageViewCenterYConstraint.active = YES;
+    
+    NSLayoutConstraint *secondImageViewCenterYConstraint = [NSLayoutConstraint constraintWithItem: secondImageView
+                                                                                        attribute:NSLayoutAttributeCenterY
+                                                                                        relatedBy:NSLayoutRelationEqual
+                                                                                           toItem:self.scrollView
+                                                                                        attribute:NSLayoutAttributeCenterY
+                                                                                       multiplier:1.0
+                                                                                         constant:0.0];
+    secondImageViewCenterYConstraint.active = YES;
+    
+    NSLayoutConstraint *thirdImageViewCenterYConstraint = [NSLayoutConstraint constraintWithItem: thirdImageView
+                                                                                       attribute:NSLayoutAttributeCenterY
+                                                                                       relatedBy:NSLayoutRelationEqual
+                                                                                          toItem:self.scrollView
+                                                                                       attribute:NSLayoutAttributeCenterY
+                                                                                      multiplier:1.0
+                                                                                        constant:0.0];
+    thirdImageViewCenterYConstraint.active = YES;
 }
 
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView {
 //    con
 //    [self.scrollView scrollRectToVisible: animated:YES];
-}
-
-- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
     
 }
 
-- (void)scrollViewDidEndZooming:(UIScrollView *)scrollView withView:(UIView *)view atScale:(CGFloat)scale {
-    
-}
-
+//- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
+//
+//}
+//
+//- (void)scrollViewDidEndZooming:(UIScrollView *)scrollView withView:(UIView *)view atScale:(CGFloat)scale {
+//
+//}
+//
 @end
