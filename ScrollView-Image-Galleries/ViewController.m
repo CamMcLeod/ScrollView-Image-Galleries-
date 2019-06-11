1589b22b5a52f6c967e0f67bce539cc1dffcfc2d
Cam McLeod <cammcleod3@gmail.com>
2019-06-10 17:40:06 -0700

    added images to views, scroll view to view controller in storyboard

--- ScrollView-Image-Galleries/ViewController.m
+++ ScrollView-Image-Galleries/ViewController.m
@@ -10,6 +10,8 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+
 @end
 
 @implementation ViewController
@@ -17,7 +19,39 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    _scrollView = self.scrollView;
+    
+    self.scrollView.delegate = self;
+    
+    self.scrollView.maximumZoomScale = 1.0;
+    self.scrollView.minimumZoomScale = 0.5;
+    
+    UIImageView *firstImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed: @"Lighthouse-in-Field"]];
+    
+    UIImageView *secondImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed: @"Lighthouse-night"]];
+    
+    UIImageView *thirdImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed: @"Lighthouse-zoomed"]];
+    
+    [self.scrollView addSubview:firstImageView];
+    [self.scrollView addSubview:secondImageView];
+    [self.scrollView addSubview:thirdImageView];
+    
+    secondImageView.frame = CGRectOffset(secondImageView.frame, firstImageView.frame.size.width, 0);
+    thirdImageView.frame = CGRectOffset(thirdImageView.frame, secondImageView.frame.size.width, 0);
+    NSLog(@"%@", NSStringFromCGSize( self.scrollView.contentSize));
 }
 
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+//    con
+//    [self.scrollView scrollRectToVisible: animated:YES];
+}
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
+    
+}
+
+- (void)scrollViewDidEndZooming:(UIScrollView *)scrollView withView:(UIView *)view atScale:(CGFloat)scale {
+    
+}
 
 @end
